c6fd2da52246155a0ba0de201fef32697a6176cc
yanghao <yanghao@xgd.com>
2021-07-22 15:25:49 -0700

    sync

--- tools/woh.py
+++ tools/woh.py
@@ -40,8 +40,52 @@ def init_cli(verbose_output=None):
             super(Action,self).__init__(name,**kwargs)
 
     class CLI(click.MultiCommand):
-        def __init__(self):
-            pass
+        def __init__(self, all_actions=None,verbose_output=None, help=None):
+            super(CLI,self).__init__(
+                chain=True,
+                invoke_without_command=True,
+                result_callback=self.execute_tasks,
+                context_settings={'max_content_width': 140},
+                help=help,
+            )
+            self._actions = {}
+
+    @click.command(
+        add_help_option=False,
+        context_settings={
+            'allow_extra_args':True,
+            'ignore_unknown_options':True
+        },
+    )
+    @click.option('-C', '--project-dir', default=os.getcwd(), type=click.Path())
+    def parse_project_dir(project_dir):
+        return realpath(project_dir)
+    project_dir = parse_project_dir(standalone_mode=False,complete_var='_WOH.PY_COMPLETE_NOT_EXISTING')
+
+    all_actions = {}
+    woh_py_extensions_path = os.path.join(os.environ['WOH_PATH'], 'tools', 'woh_py_actions')
+    extensions_dirs = [realpath(woh_py_extensions_path)]
+    extra_paths = os.environ.get('WOH_EXTRA_ACTIONS_PATH')
+    if extra_paths is not None:
+        for path in extra_paths.split(';'):
+            path = realpath(path)
+            if path not in extensions_dirs:
+                extensions_dirs.append(path)
+
+    extensions = {}
+    for directory in extensions_dirs:
+        if directory and not os.path.exists(directory):
+            print_warning('WARNING: Directory with woh.py extensions doesn\'t exist:\n    %s' % directory)
+            continue
+
+        sys.path.append(directory)
+
+    cli_help = (
+        'WOH CLI build management tool. '
+        'For commands that are not known to woh.py an attempt to execute it as a build system target will be made.')
+
+    return CLI(help=cli_help, verbose_output=verbose_output,all_actions=all_actions)
+
 
 def check_environment():
     pass
